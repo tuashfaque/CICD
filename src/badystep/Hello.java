2210bbc90b6763675e86390c97c93e61764dc12d
tashfaque <tashfaque@ael-bd.com>
2019-08-22 19:23:24 +0600

    Commit test02
    Commit testing for pull request

---
+++ src/badystep/Hello.java
@@ -0,0 +1,15 @@
+package badystep;
+
+import javax.print.DocFlavor.STRING;
+
+public class Hello {
+	public static String message = "Hello baby!";
+	public static void main(String[] args) {
+		
+		
+		System.out.println(message);
+		
+
+	}
+
+}
---
+++ src/badystep/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author tashfaque
+ *
+ */
+package badystep;
