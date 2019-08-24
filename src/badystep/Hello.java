e8efe170b43b6d4bcebccfa3746742f769dd3811
tashfaque <tashfaque@ael-bd.com>
2019-08-24 13:22:05 +0600

    Push test

--- src/badystep/Hello.java
+++ src/badystep/Hello.java
@@ -2,6 +2,7 @@ package badystep;
 
 import javax.print.DocFlavor.STRING;
 
+
 public class Hello {
 	public static String message = "Hello baby!";
 	public static void main(String[] args) {
---
+++ src/mvnInstallationTest/mvnTest.java
@@ -0,0 +1,10 @@
+package mvnInstallationTest;
+
+public class mvnTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
